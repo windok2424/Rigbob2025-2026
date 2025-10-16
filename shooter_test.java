a968078f5e73e26e150bab8dc1ec03c3e85e783e
windok2424 <121835192+windok2424@users.noreply.github.com>
2025-10-16 10:44:28 -0700

    Add files via upload

---
+++ shooter_test.java
@@ -0,0 +1,67 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.hardware.limelightvision.LLFieldMap;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.LLResultTypes;
+import com.qualcomm.hardware.limelightvision.LLResultTypes.*;
+import com.qualcomm.hardware.limelightvision.LLStatus;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+
+import java.util.List;
+
+@TeleOp(name = "shooter Test")
+public class shooter_test extends LinearOpMode{
+    DcMotorEx shootup;
+    DcMotorEx shootdown;
+
+    DcMotorEx FL;
+    DcMotorEx BL;
+    DcMotorEx FR;
+    DcMotorEx BR;
+
+    public void runOpMode() throws InterruptedException{
+        waitForStart();
+
+        shootup = hardwareMap.get(DcMotorEx.class, "shootup");
+        shootdown = hardwareMap.get(DcMotorEx.class, "shootdown");
+
+
+        FL = hardwareMap.get(DcMotorEx.class, "FL");
+        BL = hardwareMap.get(DcMotorEx.class, "BL");
+        FR = hardwareMap.get(DcMotorEx.class, "FR");
+        BR = hardwareMap.get(DcMotorEx.class, "BR");
+
+        while(opModeIsActive()){
+
+            double y = -gamepad1.left_stick_y;
+            double x = gamepad1.left_stick_x;
+            double rx = -gamepad1.right_stick_x;
+
+            boolean slowMode = gamepad1.left_stick_button;
+
+            double powerFL = (-y - x + rx);
+            double powerBL = (y - x - rx);
+            double powerFR = (y - x + rx);
+            double powerBR = (-y - x - rx);
+
+            FL.setPower(powerFL);
+            BL.setPower(powerBL);
+            FR.setPower(powerFR);
+            BR.setPower(powerBR);
+
+
+            shootup.setPower(-1);
+            shootdown.setPower(-1);
+        }
+
+    }
+
+}
